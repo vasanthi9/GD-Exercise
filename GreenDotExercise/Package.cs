c759bdca1c0c939894a42b1078251e631838b7b1
Vasanthi K <vasanthik@gmail.com>
2018-03-05 14:16:43 -0800

    First take. Added unit tests

---
+++ GreenDotExercise/Package.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenDotExercise
+{
+    public class Package
+    {
+		public string Name { get; set; }
+		public ICollection<string> Dependencies { get; set; }		
+
+	}
+}
---
+++ GreenDotExercise/Packager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenDotExercise
+{
+	public class Packager
+	{
+		Dictionary<string, List<string>> _source = new Dictionary<string, List<string>>();
+
+		public Packager()
+		{
+
+		}
+
+		public IList<string> InstallPackages(IEnumerable<Package> source)
+		{
+			var sorted = new List<string>();
+			var visited = new Dictionary<string, bool>();
+
+			foreach (var package in source)
+			{
+				_source.Add(package.Name, package.Dependencies.ToList());
+			}
+
+			foreach (var item in _source)
+			{
+				Visit(item.Key, item.Value, sorted, visited);
+			}
+
+			return sorted;
+		}
+
+		private void Visit(string itemName, ICollection<string> dependencies, List<string> sorted, Dictionary<string, bool> visited)
+		{
+			bool inProcess;
+			var alreadyVisited = visited.TryGetValue(itemName, out inProcess);
+
+			if (alreadyVisited)
+			{
+				if (inProcess)
+				{
+					throw new ArgumentException("Cyclic dependency found.");
+				}
+			}
+			else
+			{
+				visited[itemName] = true;
+
+				if (dependencies != null)
+				{
+					foreach (var dependency in dependencies)
+					{
+							Visit(dependency, GetDependencies(dependency), sorted, visited);
+					}
+				}
+
+				visited[itemName] = false;
+				sorted.Add(itemName);
+			}
+		}
+
+		private ICollection<string> GetDependencies(string itemName)
+		{
+			List<string> dependencies = new List<string>();
+
+			if (_source.ContainsKey(itemName))
+				dependencies = _source[itemName];
+
+			return dependencies;
+		}
+	}
+}
